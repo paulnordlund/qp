477badf1cb420a1e1699ba21dca40474affdcfe6
Paul Nordlund <paulnordlund@gmail.com>
2016-03-17 15:36:13 -0500

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+/*  remove side navigaton on small screens
+*/
+
+function sideNav() {
+  if ($(window).width() < 750) {
+    $('.off-canvas-wrap').removeClass('move-right');
+    $('.left-off-canvas-toggle').show();
+  } else {
+    $('.off-canvas-wrap').addClass('move-right');
+    $('.left-off-canvas-toggle').hide();
+  }  
+}
+
+$(window).resize(function() {
+  sideNav();
+});
+
+
+/*  add to cart
+*/
+
+$('.add-to-order-button').on('click', function() {
+	console.log('add to cart');
+});
+
---
+++ js/jq-ajax.js
@@ -0,0 +1,38 @@
+$('aside a').on('click', function(e) {
+  e.preventDefault();
+  var url = this.href;                                      // URL to load
+  var $content = $('#content');                             // Cache selection
+
+  $('aside a.active').removeClass('active');                // Update links
+  $(this).addClass('active');
+  $('#container').remove();                                 // Remove content
+
+  $.ajax({
+    type: "GET",                                            // GET or POST
+    url: url,                                               // Path to file
+    timeout: 2000,                                          // Waiting time
+    beforeSend: function() {                                // Before Ajax 
+      $content.append('<div id="load">Loading</div>');      // Load message
+    },
+    complete: function() {                                  // Once finished
+      $('#load').remove();                                  // Clear message
+    },
+    success: function(data) {                               // Show content
+      $content.html( $(data).find('#container') ).hide().fadeIn(400);
+    },
+    error: function() {                                     // Show error msg 
+      $content.html('<div class="container">Please try again soon.</div>');
+    }
+  });
+
+});
+
+/*
+ Here is some further information on what happens when the call is successful:
+
+ success: function(data) {...  The data parameter holds the Ajax response with the new content
+ $content was stored on line 10 it is the element whose id attribut has a value of content
+ $content.html() updates the HTML inside this element
+ $(data) creates a jQuery object containing the response.
+ .find('#container') gets the container element from that response hides it and fades it in
+*/
